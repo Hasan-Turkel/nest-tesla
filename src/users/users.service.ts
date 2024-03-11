aecebc76d4e4c9d16612576603b70c2953dfedd6
hasan <mmhotmail>
2024-03-11 18:39:00 +0300

    users

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './users.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+
+    constructor(@InjectModel(User.name) private UserModel : Model<UserDocument>) {}
+
+  private readonly users=[]
+
+    async findAll() {
+        return await this.UserModel.find()
+    }
+    async findOne(id) {
+        return await this.UserModel.findById(id)
+    }
+
+    async create(user) {
+        const newUser = new this.UserModel(user)
+        return await newUser.save()
+    }
+    async update(id, user) {
+         
+        return await this.UserModel.findByIdAndUpdate(id, user, {new:true})
+    }
+    async delete(id) {
+         
+        return await this.UserModel.findByIdAndDelete(id)
+    }
+}
