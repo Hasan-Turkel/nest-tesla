aa9026e0199eeb4c3f60aa03156ab6c66bb319a8
hasan <mmhotmail>
2024-03-20 17:40:16 +0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,18 @@ import { TokensModule } from './tokens/tokens.module';
 import { AuthModule } from './auth/auth.module';
 import { CarsModule } from './cars/cars.module';
 import { ExtrasModule } from './extras/extras.module';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [UsersModule,TokensModule, AuthModule, CarsModule, ExtrasModule, TokensModule, MongooseModule.forRoot('mongodb://localhost:27017/tesla',  {
-    autoIndex: true,
-  })],
+  imports: [UsersModule,TokensModule, AuthModule, CarsModule, ExtrasModule, TokensModule,  ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB'),
+      }),
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 
 async function bootstrap() {
 
-  const port = 3000
-
   const app = await NestFactory.create(AppModule, {cors:true});
   app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+const port = configService.get<number>('PORT');
   await app.listen(port, ()=> {Logger.log(port)});
 }
 
